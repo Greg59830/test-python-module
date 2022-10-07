172f987079ce0df0cf28acd38220420105cd3630
Greg59830 <108268555+Greg59830@users.noreply.github.com>
2022-10-07 20:43:16 +0200

    Add files via upload

---
+++ min.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+def min_misson ():
+  list1 = []
+  n = int(input("Enter number of elements in list: "))
+  for i in range(1, n+1):
+    element=int(input("Enter elements: "))
+    list1.append(element)
+  print("Smallest element is: ", min(list1))
+
+print(min_misson())
